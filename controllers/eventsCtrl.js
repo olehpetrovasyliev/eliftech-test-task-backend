a0adcf227476a5dc0c0a45be14b43d1c88a34fb1
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2024-09-19 22:52:09 +0300

    add pagination

--- controllers/eventsCtrl.js
+++ controllers/eventsCtrl.js
@@ -2,7 +2,12 @@ const Event = require("../models/Event");
 const { ctrlWrapper } = require("../helpers");
 
 const getAllEvents = async (req, res, next) => {
-  const result = await Event.find({}, "-createdAt");
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const result = await Event.find({}, "-createdAt")
+    .limit(Number(limit))
+    .skip((page - 1) * limit);
   res.json(result);
 };
 
