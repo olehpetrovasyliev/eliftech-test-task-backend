41aed4a27349a7005d37bad53b34f121e3316d53
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2024-09-19 23:25:18 +0300

    add sorting events

--- controllers/eventsCtrl.js
+++ controllers/eventsCtrl.js
@@ -4,10 +4,15 @@ const { ctrlWrapper } = require("../helpers");
 const getAllEvents = async (req, res, next) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
+  const sortBy = req.query.sortBy || "eventDate";
+  const sortDirection = req.query.sortDirection === "desc" ? -1 : 1;
+
+  const sortOptions = { [sortBy]: sortDirection };
 
   const result = await Event.find({}, "-createdAt")
     .limit(Number(limit))
-    .skip((page - 1) * limit);
+    .skip((page - 1) * limit)
+    .sort(sortOptions);
   res.json(result);
 };
 
