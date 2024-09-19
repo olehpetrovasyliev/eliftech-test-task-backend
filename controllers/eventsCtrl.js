5bdae6708cf6dfe626774be297b959f81d1d47e1
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2024-09-19 23:42:34 +0300

    add fetching event info by id

--- controllers/eventsCtrl.js
+++ controllers/eventsCtrl.js
@@ -9,13 +9,38 @@ const getAllEvents = async (req, res, next) => {
 
   const sortOptions = { [sortBy]: sortDirection };
 
-  const result = await Event.find({}, "-createdAt")
+  const result = await Event.find(
+    {},
+    { "-createdAt": 0, "-updatedAt": 0, "-participants": 0 }
+  )
     .limit(Number(limit))
     .skip((page - 1) * limit)
     .sort(sortOptions);
   res.json(result);
 };
 
+const getEventById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Event.findById(id, {
+    createdAt: 0,
+    updatedAt: 0,
+  });
+  if (!result) {
+    throw HttpError(404, `Event with id ${id} not found`);
+  }
+
+  const modifiedEvent = {
+    ...result.toObject(),
+    participants: result.participants.map((participant) => ({
+      _id: participant._id,
+      name: participant.name,
+      email: participant.email,
+    })),
+  };
+  res.json(modifiedEvent);
+};
+
 module.exports = {
   getAllEvents: ctrlWrapper(getAllEvents),
+  getEventById: ctrlWrapper(getEventById),
 };
--- routes/api/events.js
+++ routes/api/events.js
@@ -4,5 +4,6 @@ const { eventsCtrl } = require("../../controllers");
 const router = express.Router();
 
 router.get("/", eventsCtrl.getAllEvents);
+router.get("/:id", eventsCtrl.getEventById);
 
 module.exports = router;
